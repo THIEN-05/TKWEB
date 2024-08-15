13c0d0c0097470bee11c98b9b77163e3bb09143e
THIEN-05 <thienletam734@gmail.com>
2024-08-15 20:04:02 +0700

    update
    update

---
+++ javasrip/test.js
@@ -0,0 +1,6 @@
+document.getElementById("show-login").addEventListener("click", function() {
+    document.querySelector(".popup").style.display = "flex";
+});
+document.querySelector(".close").addEventListener("click", function() {
+    document.querySelector(".popup").style.display = "none";
+});
