2af5611430318e606c9465eb82ab1e25ffb6915d
THIEN-05 <thienletam734@gmail.com>
2024-08-17 14:05:02 +0700

    het ten dat
    thua nha

--- javasrip/test.js
+++ javasrip/test.js
@@ -1,6 +1,10 @@
+window.onload=function(){
+
+
 document.getElementById("show-login").addEventListener("click", function() {
     document.querySelector(".popup").style.display = "flex";
 });
 document.querySelector(".close").addEventListener("click", function() {
     document.querySelector(".popup").style.display = "none";
 });
+}
