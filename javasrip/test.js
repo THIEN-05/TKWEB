c1317c5d4fcbb0cc6db84453e27d2bd2ab35ccce
THIEN-05 <thienletam734@gmail.com>
2024-08-19 21:40:27 +0700

    update
    ok

--- javasrip/test.js
+++ javasrip/test.js
@@ -6,27 +6,41 @@ window.onload = function () {
     for (let f of addbooks)
         f.classList.add("animate__pulse");
 
-
+//dang nhap
     document.getElementById("show-login").addEventListener("click", function () {
         document.querySelector(".popup").style.display = "flex";
     });
     document.querySelector(".close").addEventListener("click", function () {
         document.querySelector(".popup").style.display = "none";
     });
-
+//wow
     wow = new WOW(
         {
-        boxClass:     'wow',     
-        animateClass: 'animate__animated', 
-        offset:       0,          
-        mobile:       true,       
-        live:         true       
-      }
-      )
-      wow.init();
+            boxClass: 'wow',
+            animateClass: 'animate__animated',
+            offset: 0,
+            mobile: true,
+            live: true
+        }
+    )
+    wow.init();
+    //show ảnh
+    let myimg = document.getElementsByTagName("img");
+    let myim = document.getElementById("popimg");
+    let popup = document.querySelector(".popupanh");
+    for (let im of myimg)
+        im.onclick = function () {
+            myim.src = this.src;
+            popup.classList.add("show");
 
+        }
+    let close=document.querySelector(".popupanh .closeanh");
+    close.onclick=function(){
+        popup.classList.remove("show");
+    }
 }
-window.onscroll = function() {scrollFunction()};
+//go to top
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
